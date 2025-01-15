db6c4a843d2d2133781dba112c09253b711a1acc
Mateo Gray <mateo2@Mateos-Mac-Studio.local>
2025-01-14 17:59:21 -0700

    Fixed improperly not added unpaired base values in traceback

--- bash.sh
+++ bash.sh
@@ -1,15 +1,16 @@
 input=$1
-file="/home/mgray7/output2/fasta/$input.txt";
-file2="/home/mgray7/output2/structures/$input.txt";
+file="/Users/mateo2/Documents/Code/output2/fasta/$input.txt";
+file2="/Users/mateo2/Documents/Code/output2/structures/$input.txt";
 exec 5<$file
 exec 6<$file2
+i=0
 while read line1 <&5 && read line2 <&6; do
 
 if ((i % 2 == 1))  
-
+# echo $line1 
 then
-echo $line1 > out1.txt
-echo $line2 >> out1.txt
+echo $line1 > "out1.txt"
+echo $line2 >> "out1.txt"
 # gtime -o out.txt -f "%e\t%M" ./build/Spark -P "/Users/mateo2/Documents/Code/Spark/src/params/parameters_DP09_Vienna.txt" -d1 -r $line2  $line1;
 # gtime -o out.txt -f "%e\t%M" /Users/mateo2/Documents/Code/HFold/HFold -s $line1 -r $line2
 
@@ -17,19 +18,21 @@ echo $line2 >> out1.txt
 # ./build/Spark -P "/Users/mateo2/Documents/Code/Spark/src/params/parameters_DP09_Vienna.txt" -d1 -r $line2  $line1 > "/Users/mateo2/Documents/Code/Spark/out.txt";
 # echo $line > out2.txt;
 # gtime -o out.txt -f "%e\t%M" ./build/CParty -d2 -r $line2 $line1
-# ./build/CParty -d2 -r $line2 $line1 > "/home/mgray7/CParty/out.txt"
-./build/CParty -d2 -r $line2 $line1
-# /home/mgray7/ViennaRNA-2.6.4/src/bin/RNAfold --constraint<out1.txt --enforceConstraint --noPS --noDP -d2 -p out1.txt > out.txt
+# echo $line1
+# echo $line2
+# /Users/mateo2/Documents/Code/includes/Vienna/bin/RNAfold -d2 --constraint<out1.txt --enforceConstraint out1.txt -p --noPS --noDP > "out.txt"
+./build/CParty -d2 -r $line2 $line1 >> "/Users/mateo2/Documents/Code/output2/pkfree/CParty/$input.txt"
+# ./build/CParty -d2 -r $line2 $line1 > "/Users/mateo2/Documents/Code/CParty/out.txt"
+# /Users/mateo2/Documents/Code/ViennaRNA-2.6.4/src/bin/RNAfold --noPS --noDP -d2 -p out1.txt > out.txt
 # /Users/mateo2/Documents/Code/HFold/HFold -s $line1 -r $line2 > "/Users/mateo2/Documents/Code/Spark/out.txt";
 # ../util/a.o
-# cat "/Users/mateo2/Documents/Code/Spark/out.txt" >> "/Users/mateo2/Documents/Code/output/proof/Spark/$input.txt"
+# cat "/Users/mateo2/Documents/Code/CParty/out.txt" >> "/Users/mateo2/Documents/Code/output2/pkfree/$input.txt"
 # cat "/Users/mateo2/Documents/Code/Spark/out.txt" >> "/Users/mateo2/Documents/Code/output/proof/HFold/$input.txt"
 # cat "/home/mgray7/CParty/out2.txt" >> "/home/mgray7/output2/energies/RNAFold/$input.txt"
 
-cat "/home/mgray7/CParty/out.txt" >> "/home/mgray7/output2/energies/CParty/$input.txt"
 # cat "/Users/mateo2/Documents/Code/CParty/out2.txt" >> "/Users/mateo2/Documents/Code/output2/energies/RNAFold/$input.txt"
 
-# cat "/Users/mateo2/Documents/Code/Spark/out2.txt" >> "/Users/mateo2/Documents/Code/output2/energies/RNAFold/$input.txt"
+# cat "/Users/mateo2/Documents/Code/CParty/out2.txt" >> "/Users/mateo2/Documents/Code/output2/pkfree/RNAFold/$input.txt"
 
 # echo /usr/bin/time -o out.txt -f "%e\t%M" ./build/Spark -P "/home/mgray7/Spark/src/params/parameters_DP09_Vienna.txt" -p -d1 -r \"$line2\"  $line1;
 # echo $line2
